717fab2668af7eb8745cd8aa51eb995370e05f63
Onur Alptürk <55660479+oalptu1@users.noreply.github.com>
2026-08-15 20:51:20 +0300

    Update run_analysis_6TZ6.sh

--- scripts/amber/run_analysis_6TZ6.sh
+++ scripts/amber/run_analysis_6TZ6.sh
@@ -9,7 +9,7 @@ set -e
 INPUT_PDB="6TZ6_FK506.pdb"
 LIGAND="FK5"
 LIGAND_RESIDUE="619"
-PREFIX="6TZ6_FK506"
+PREFIX="FK506"
 
 # ============================================================
 # FILE NAMES
